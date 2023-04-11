3b8471f19717dc3906e58770d6ce783ce9803561
Paolo <paololongo.git@gmail.com>
2023-04-11 18:16:18 +0200

    commit

--- terraform/inputs.tf
+++ terraform/inputs.tf
@@ -1,12 +1,19 @@
 variable "AWS_REGION" {
-  type = string
+  type        = string
+  description = "AWS region in which deploy the service."
 }
 
 variable "WORKSPACE_PREFIX" {
-  type = string
+  type        = string
+  description = "Prefix of the workspaces."
 }
 
 variable "ENV" {
-  type           = string
-  allowed_values = ["prod", "dev"]
+  type        = string
+  description = "Define if the deploy for for 'prod' or 'dev'."
+
+  validation {
+    condition     = contains(["prod", "dev"], var.ENV)
+    error_message = "The ENV variable must be 'prod' or 'dev'."
+  }
 }
