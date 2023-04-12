54341a4b6d0b9c826bb8f09816f72075586b8c1a
Paolo <paololongo.git@gmail.com>
2023-04-12 21:38:52 +0200

    x

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,7 @@ resource "aws_s3_bucket" "lambda_bucket" {
   bucket = random_pet.lambda_bucket_name.id
 }
 
+
 resource "aws_s3_bucket_acl" "bucket_acl" {
   bucket = aws_s3_bucket.lambda_bucket.id
   acl    = "private"
