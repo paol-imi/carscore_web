bd7d2bbb11d60531f5c532fff0e9d4fd04182d96
Paolo <paololongo.git@gmail.com>
2023-04-12 23:58:00 +0200

    commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,11 @@
 provider "aws" {
   region = var.AWS_REGION
   # Since we are using Terraform Cloud, the configuration lives in env variable
-  access_key = "AKIAXPKXG6F2MFPZW6OF"
-  secret_key = "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"
+  assume_role {
+    role_arn     = "arn:aws:iam::513968370036:role/GithubWebIdentity"
+    session_name = "SESSION_NAME"
+  }
 }
-
 
 terraform {
 
