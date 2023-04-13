acb05eee2abf28055c7161b8ebf5889fff2ae724
Paolo <paololongo.git@gmail.com>
2023-04-13 10:17:54 +0200

    c

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,11 @@
 provider "aws" {
   region = var.AWS_REGION
   # Since we are using Terraform Cloud, the configuration lives in env variable
-  assume_role {
-    role_arn     = "arn:aws:iam::513968370036:role/GithubWebIdentity"
-    session_name = "SESSION_NAME"
-  }
+  # assume_role {
+  #   role_arn     = "arn:aws:iam::123456789012:role/ROLE_NAME"
+  #   session_name = "SESSION_NAME"
+  #   external_id  = "EXTERNAL_ID"
+  # }
 }
 
 terraform {
