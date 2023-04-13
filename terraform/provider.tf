6bf2c38386ec3a429bb3b6ac7f28ffb2445e290e
Paolo <paololongo.git@gmail.com>
2023-04-13 10:38:35 +0200

    x

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,11 +1,11 @@
 provider "aws" {
   region = var.AWS_REGION
   # Since we are using Terraform Cloud, the configuration lives in env variable
-  assume_role {
-    role_arn = "arn:aws:iam::513968370036:role/TerraformCloudRole"
-    #   session_name = "SESSION_NAME"
-    #   external_id  = "EXTERNAL_ID"
-  }
+  #assume_role {
+  #role_arn = "arn:aws:iam::513968370036:role/TerraformCloudRole"
+  #   session_name = "SESSION_NAME"
+  #   external_id  = "EXTERNAL_ID"
+  #}
 }
 
 terraform {
@@ -17,17 +17,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.15.0"
+      version = "~> 4.62.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.1.0"
+      version = "~> 3.5.1"
     }
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 2.2.0"
+      version = "~> 2.3.0"
     }
   }
 
-  required_version = "~> 1.4.3"
+  required_version = "~> 1.4.5"
 }
