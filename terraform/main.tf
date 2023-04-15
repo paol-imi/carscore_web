259247ead82ac4618efdb4ec1129aea4720613fd
Paolo Longo <paololongo.git@gmail.com>
2023-04-15 19:42:21 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,7 @@ resource "aws_s3_object" "lambda_hello_world" {
 }
 
 resource "aws_lambda_function" "hello_world" {
-  function_name = "${var.BRANCH}.hello_world"
+  function_name = "${var.BRANCH}_hello_world"
 
   s3_bucket = aws_s3_bucket.lambda_bucket.id
   s3_key    = aws_s3_object.lambda_hello_world.key
