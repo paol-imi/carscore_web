4b586421bb43797b778eeab0d3d8709d031f8451
Paolo <paololongo.git@gmail.com>
2023-04-17 23:22:10 +0200

    c

--- terraform/branch-dependent/outputs.tf
+++ terraform/branch-dependent/outputs.tf
@@ -8,7 +8,7 @@ output "lambda_bucket_name" {
 output "function_name" {
   description = "Name of the Lambda function."
 
-  value = aws_lambda_function.lambda.test.function_name
+  value = aws_lambda_function.lambda["test"].function_name
 }
 
 output "base_url" {
